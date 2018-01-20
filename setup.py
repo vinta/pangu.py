95e7313c2de65606f5b4a50b6bafeaa33ee65a14
Vinta <vinta.chen@gmail.com>
2018-01-20 17:21:20 +0800

    fix format

--- setup.py
+++ setup.py
@@ -1,25 +1,43 @@
 #!/usr/bin/env python
+# coding: utf-8
 
 import os
 import sys
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
-import pangu
 
+def get_version():
+    code = None
+    path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'pangu.py',
+    )
+    with open(path) as f:
+        for line in f:
+            if line.startswith('__version__'):
+                code = line[len('__version__ = '):]
+                break
+    return eval(code)
+
+
+if sys.argv[-1] == 'wheel':
+    os.system('make clean')
+    os.system('pip install wheel')
+    os.system('python setup.py bdist_wheel')
+    sys.exit(0)
 
 if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist bdist_wheel upload')
-    sys.exit()
+    os.system('python setup.py wheel')
+    os.system('pip install twine')
+    os.system('twine upload dist/*')
+    sys.exit(0)
 
-long_description = open('README.rst').read() + '\n\n' + open('HISTORY.rst').read()
+long_description = open('README.rst').read() + '\n' + open('HISTORY.rst').read()
 
 setup(
     name='pangu',
-    version=pangu.__version__,
+    version=get_version(),
     description='Paranoid text spacing for good readability, to automatically insert whitespace between CJK (Chinese, Japanese, Korean) and half-width characters (alphabetical letters, numerical digits and symbols).',
     long_description=long_description,
     keywords='pangu space white text spacing readability',
